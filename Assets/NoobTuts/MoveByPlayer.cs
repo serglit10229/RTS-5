e42b712e52b496a2c9380483e530bfeee26f2099
Sergei Litovchenko <serglit10229@gmail.com>
2018-02-06 21:08:53 -0800

    Initial commit

---
+++ Assets/NoobTuts/MoveByPlayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class MoveByPlayer : MonoBehaviour {
+    // Selection Circle
+    public GameObject circle;
+    public NavMeshAgent agent;
+
+    
+    // Update is called once per frame
+    void Update () {
+        // Rightclicked while selected?
+        if (Input.GetMouseButtonDown(1) && circle.activeSelf) {
+            // Find out where the user clicked in the 3D world
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = false;
+                GetComponent<UnityEngine.AI.NavMeshAgent>().destination = hit.point;
+            }
+        }
+        /*
+        float dist = agent.remainingDistance;
+
+        if (dist != Mathf.Infinity && agent.pathStatus == NavMeshPathStatus.PathComplete && agent.remainingDistance == 0)
+        {
+            Debug.Log("stop");
+            GetComponent<UnityEngine.AI.NavMeshAgent>().isStopped = true;
+        }
+        */
+    }
+}
